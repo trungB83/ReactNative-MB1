5c49a2d0c455746c694e2499b4a9e278d581fd6e
Bui Quang Trung <63466120+boonreal@users.noreply.github.com>
2023-06-30 08:24:03 +0700

    init

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from "./app/views/LandingScreen/landingScreen"
+import LoginScreen from './app/views/Login/login';
+import SignUpScreen from './app/views/SignUp/signUp';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  React.useEffect(() => {
+    const checkLoginStatus = async () => {
+      const value = await AsyncStorage.getItem('isLoggedIn');
+      if (value === 'true') {
+        setIsLoggedIn(true);
+      }
+    };
+    checkLoginStatus();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isLoggedIn ? (
+          <>
+            HI
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
